1d800cfa77d94b1e59f9f1e9e73d2b9fa6e9821d
tlima1011 <thiagolima1011@gmail.com>
2026-05-16 13:32:46 -0300

    Test Main

--- compay-outsourced-employee/Program.cs
+++ compay-outsourced-employee/Program.cs
@@ -1,11 +1,48 @@
-﻿using System;
+﻿using compay_outsourced_employee.Entities;
+using System;
+using System.Globalization;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
 namespace CompanyOutsourcedEmployee
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<Employee> list = new List<Employee>();
+
+            Console.WriteLine("Enter the number of employees: ");
+            int n = int.Parse(Console.ReadLine()); 
+
+            for(int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Employee # {i} data:");
+                Console.Write("Outsourced(y / n) ? ");
+                char oe = char.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Hours: ");
+                int hours = int.Parse(Console.ReadLine());
+                Console.Write("Value per hour: ");
+                double valuePerHour = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture); 
+                if(oe == 'n' || oe == 'N')
+                {
+                    list.Add(new Employee(name, hours, valuePerHour)); 
+                }
+                else
+                {
+                    Console.Write("Additional charge: ");
+                    double additional = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    list.Add(new OutsourcedEmployee(name, hours, valuePerHour, additional));  
+                }
+
+
+
+
+            }
+            
+
+
         }
     }
 }
