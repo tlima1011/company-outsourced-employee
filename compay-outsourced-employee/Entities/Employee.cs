427df65c61bafd3d38f9f7466070cf8e81533377
tlima1011 <thiagolima1011@gmail.com>
2026-05-16 12:38:44 -0300

    Entity Employee Created

---
+++ compay-outsourced-employee/Entities/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace compay_outsourced_employee.Entities
+{
+    internal class Employee
+    {
+        public string Name { get; set; }
+        public int Hours { get; set; }
+        public double ValuePerHour { get; set; }
+
+
+        public double payment()
+        {
+            return Hours * ValuePerHour; 
+        }
+    }
+}
