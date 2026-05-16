2baf99e0d21e663a5cc912103a3922102e059c3d
tlima1011 <thiagolima1011@gmail.com>
2026-05-16 12:34:16 -0300

    Projeto Criado

---
+++ compay-outsourced-employee/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace CompanyOutsourcedEmployee
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
